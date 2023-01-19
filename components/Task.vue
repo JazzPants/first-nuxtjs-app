41e46f26a5e51c945d9af9c2b2f52e68086a2077
Leighton <leighton.l14@gmail.com>
2023-01-19 14:19:39 +1100

    4th Commit - Added basic UI file with addTask method (index.vue) and TASK file component for other methods (toggleDone, removeTask)

---
+++ components/Task.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="`task ${task.done ? 'is-complete' : ''}`">
+        <div class="content">{{ task.content }}</div>
+        <button @click="toggleDone">{{ task.done ? 'Undo' : 'Complete' }}</button>
+        <button @click="removeTask" class="delete">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['task'],
+    methods: {
+        toggleDone() {
+            this.$store.commit('TOGGLE_TASK', this.task)
+        },
+        removeTask(task) {
+            this.$store.commit('REMOVE_TASK', this.task)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
